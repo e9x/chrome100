d02d23a97e8c581ed321773458ae2e9b5591f8c5
David Reed <reedswork@proton.me>
2022-10-28 17:58:50 -0400

    add more sorting options

--- packages/website/src/board.tsx
+++ packages/website/src/board.tsx
@@ -2,8 +2,12 @@ import type {
   cros_target,
   cros_recovery_image_db,
   cros_brand,
-} from "chrome-versions";
-import { getRecoveryURL } from "./chrome-versions.js";
+} from "./chrome-versions.js";
+import {
+  getRecoveryURL,
+  parsePlatformVersion,
+  parseChromeVersion,
+} from "./chrome-versions.js";
 import { render, h, Fragment } from "https://unpkg.com/preact@latest?module";
 import {
   useEffect,
@@ -18,9 +22,49 @@ interface BoardData {
   brands: cros_brand[];
 }
 
+type SortOrder = "lastModified" | "chrome" | "platform";
+
+const sortImages = (
+  sortOrder: SortOrder,
+  sortReverse: boolean,
+  images: Readonly<cros_recovery_image_db[]>
+) => {
+  const sorted = [...images];
+
+  sorted.sort((a, b) => {
+    switch (sortOrder) {
+      case "chrome": {
+        const av = parseChromeVersion(a.chrome);
+        const bv = parseChromeVersion(b.chrome);
+        return bv[0] < av[0] && bv[1] < av[1] && bv[2] < av[2] && bv[3] < av[3]
+          ? 1
+          : 0;
+      }
+      case "platform": {
+        const av = parsePlatformVersion(a.platform);
+        const bv = parsePlatformVersion(b.platform);
+        return bv[0] < av[0] && bv[1] < av[1] && bv[2] < av[2] ? 1 : 0;
+      }
+      case "lastModified":
+        return (
+          new Date(b.last_modified).getTime() -
+          new Date(a.last_modified).getTime()
+        );
+      default:
+        throw new Error(`Unknown order ${sortOrder}`);
+    }
+  });
+
+  if (sortReverse) sorted.reverse();
+
+  return sorted;
+};
+
 const BoardPage = () => {
   const board = new URLSearchParams(location.search).get("board");
   const [boardData, setBoardData] = useState<null | BoardData>(null);
+  const [sortReverse, setSortReverse] = useState(false);
+  const [sortOrder, setSortOrder] = useState<SortOrder>("lastModified");
 
   useEffect(() => {
     if (!board) return;
@@ -66,6 +110,23 @@ const BoardPage = () => {
         ))}
       </ul>
       <h2>Recovery Images</h2>
+      <h3>Sort</h3>
+      <select
+        onChange={(e) => setSortOrder(e.currentTarget.value as SortOrder)}
+      >
+        <option value="lastModified">Last Modified</option>
+        <option value="chrome">Chrome Version</option>
+        <option value="platform">Platform Version</option>
+      </select>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          onChange={(e) => setSortReverse(e.currentTarget.checked)}
+        />{" "}
+        Reverse
+      </label>
+      <h3>Boards</h3>
       {boardData.images.length ? (
         <table>
           <thead>
@@ -75,13 +136,8 @@ const BoardPage = () => {
             <th />
           </thead>
           <tbody>
-            {boardData.images
-              .sort(
-                (a, b) =>
-                  new Date(b.last_modified).getTime() -
-                  new Date(a.last_modified).getTime()
-              )
-              .map((img, i) => (
+            {sortImages(sortOrder, sortReverse, boardData.images).map(
+              (img, i) => (
                 <tr key={i}>
                   <td>{img.platform}</td>
                   <td>{img.chrome}</td>
@@ -90,7 +146,8 @@ const BoardPage = () => {
                     <a href={getRecoveryURL(img)}>Download</a>
                   </td>
                 </tr>
-              ))}
+              )
+            )}
           </tbody>
         </table>
       ) : (
