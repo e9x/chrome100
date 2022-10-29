ab0cb5dee7aaf3c098869767fe39048e9048bdac
David Reed <reedswork@proton.me>
2022-10-29 16:35:42 -0400

    use native fs

--- packages/website/build.js
+++ packages/website/build.js
@@ -1,20 +1,15 @@
 import { createRequire } from "node:module";
-import fs from "fs-extra";
+import { rm, cp, mkdir } from "node:fs/promises";
 
 const require = createRequire(import.meta.url);
 
 try {
-  await fs.rm("dist", { recursive: true, force: true });
+  await rm("dist", { recursive: true, force: true });
 } catch (err) {
   if (err?.code !== "ENOENT") throw err;
 }
-await fs.copy("public", "dist");
-await fs.mkdir("dist/static/");
-await fs.copy(
-  require.resolve("github-markdown-css"),
-  "dist/static/markdown.css"
-);
-await fs.copy(
-  require.resolve("chrome-versions"),
-  "dist/static/chrome-versions.js"
-);
+
+await cp("public", "dist", { recursive: true });
+await mkdir("dist/static/");
+await cp(require.resolve("github-markdown-css"), "dist/static/markdown.css");
+await cp(require.resolve("chrome-versions"), "dist/static/chrome-versions.js");
