f63679413fc261834c288aee0df427a5802242b2
David Reed <reedswork@proton.me>
2022-10-28 21:27:21 -0400

    use createRequire to resolve modules

--- packages/website/build.js
+++ packages/website/build.js
@@ -1,18 +1,20 @@
+import { createRequire } from "node:module";
 import fs from "fs-extra";
+
+const require = createRequire(import.meta.url);
 
 try {
   await fs.rm("dist", { recursive: true, force: true });
 } catch (err) {
   if (err?.code !== "ENOENT") throw err;
 }
-
 await fs.copy("public", "dist");
 await fs.mkdir("dist/static/");
 await fs.copy(
-  "node_modules/github-markdown-css/github-markdown.css",
+  require.resolve("github-markdown-css"),
   "dist/static/markdown.css"
 );
 await fs.copy(
-  "node_modules/chrome-versions/lib/index.js",
+  require.resolve("chrome-versions"),
   "dist/static/chrome-versions.js"
 );
